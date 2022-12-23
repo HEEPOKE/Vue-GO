b060a72cabbd18870327d203f71ae083238fe6d4
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-24 06:32:03 +0700

    Update routes.ts

--- Client/src/routes/routes.ts
+++ Client/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 const HomePage = () => import("../views/HomePage.vue");
+const ProductsListPage = () => import("../views/ProductsListPage.vue");
 const AboutPage = () => import("../views/AboutPage.vue");
 
 const routes = [
   { path: "/", name: "HomePage", component: HomePage },
+  { path: "/product", name: "ProductsListPage", component: ProductsListPage },
   { path: "/about", name: "AboutPage", component: AboutPage },
 ];
 
