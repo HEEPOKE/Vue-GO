658535c572bda8e7d7724d7cc5f1215fc7bc7f4f
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-23 16:17:47 +0700

    add tw-elements

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="bg-amber-500">go</button>
+
 </template>
 <script lang="ts">
 export default {
--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./index.css";
 import App from "./App.vue";
+import 'tw-elements';
 
 createApp(App).mount("#app");
