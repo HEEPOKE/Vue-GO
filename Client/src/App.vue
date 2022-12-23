bbe7b9678c7f595441fe9490b29daa27c18c5dd6
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-23 16:04:32 +0700

    add tailwindcss

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,29 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="bg-amber-500">go</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script lang="ts">
+export default {
+  name: "App",
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
