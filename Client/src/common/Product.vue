68d71e7250a8ea583bbb56d4692e54a21e0570be
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-12-24 06:27:45 +0700

    update productpage

--- Client/src/common/Product.vue
+++ Client/src/common/Product.vue
@@ -1,17 +1,12 @@
 <template>
-    <div>
-        <NavbarMenu />
-        <ProductList />
-    </div>
+    <ProductList />
 </template>
 <script lang="ts">
-import NavbarMenu from '../common/Navbar.vue'
 import ProductList from '../components/Product/Index.vue'
 
 export default {
     name: "ProductsListCommon",
     components: {
-        NavbarMenu,
         ProductList
     }
 }
