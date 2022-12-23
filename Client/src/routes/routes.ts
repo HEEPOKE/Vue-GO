e606b3ce067c2aba3cca891d1907a630c1226d2c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-24 01:17:16 +0700

    Update routes.ts

--- Client/src/routes/routes.ts
+++ Client/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../views/HomePage.vue";
+// import HomePage from "../views/HomePage.vue";
 import AboutPage from "../views/AboutPage.vue";
+const HomePage = () => import("../views/HomePage.vue");
 
 const routes = [
   { path: "/", name: "HomePage", component: HomePage },
