5104c36f0b565b7c8f8a9c37112fded0edd4e6c2
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-26 09:16:12 +0700

    add notfoundpage

--- Client/src/routes/routes.ts
+++ Client/src/routes/routes.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 const HomePage = () => import("../views/HomePage.vue");
 const ProductsListPage = () => import("../views/ProductsListPage.vue");
 const AboutPage = () => import("../views/AboutPage.vue");
+const NotFoundPage = () => import("../views/NotFoundPage.vue");
 
 const routes = [
   { path: "/", name: "HomePage", component: HomePage },
   { path: "/product", name: "ProductsListPage", component: ProductsListPage },
   { path: "/about", name: "AboutPage", component: AboutPage },
+  { path: "/*", name: "NotFoundPage", component: NotFoundPage },
 ];
 
 const router = createRouter({
