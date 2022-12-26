f5885d7d5f3e8c6f9a4c530957f0ea12dcd8247c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-26 12:45:43 +0700

    Update Header.vue

--- Client/src/components/Product/Header.vue
+++ Client/src/components/Product/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center bg-gray-50 text-gray-800 py-20 px-6">
-    <h1 class="text-5xl font-bold mt-0 mb-6">Product List</h1>
+  <div class="text-center text-gray-800">
+    <h1 class="text-5xl font-bold mt-5">Product List</h1>
   </div>
 </template>
 <script lang="ts">
