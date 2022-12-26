39ed49219f07dd75a7f2cbb73c4debfec3cb3938
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-26 11:50:58 +0700

    add 404 page

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
-import "./index.css";
 import App from "./App.vue";
 import router from "./routes/routes";
 import "tw-elements";
+import "./index.css";
+import './assets/css/NotFound.css'
 
 createApp(App).use(router).mount("#app");
