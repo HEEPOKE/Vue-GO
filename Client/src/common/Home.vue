a1c8dec7ac55f964411a4df7278968d6b98df670
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-27 16:04:04 +0700

    setup darkmode

--- Client/src/common/Home.vue
+++ Client/src/common/Home.vue
@@ -1,8 +1,23 @@
 <template>
-  <button>click me</button>
+  <button class="text-white bg-blue-500 hover:bg-blue-700 rounded-full py-2 px-4" :class="{ 'dark-mode': darkMode }"
+    @click="toggleDarkMode">
+    Toggle Dark Mode
+  </button>
 </template>
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: "Home",
-};
+  data() {
+    return {
+      darkMode: false,
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      this.darkMode = !this.darkMode;
+    },
+  },
+})
 </script>
