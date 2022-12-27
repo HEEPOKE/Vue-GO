3a75023d23a11aa074c71265b06b7b5b41ebc7a1
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-27 16:14:13 +0700

    setup font-awesome

--- Client/src/common/Home.vue
+++ Client/src/common/Home.vue
@@ -1,23 +1,10 @@
 <template>
-  <button class="text-white bg-blue-500 hover:bg-blue-700 rounded-full py-2 px-4" :class="{ 'dark-mode': darkMode }"
-    @click="toggleDarkMode">
-    Toggle Dark Mode
-  </button>
+  home
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: "Home",
-  data() {
-    return {
-      darkMode: false,
-    };
-  },
-  methods: {
-    toggleDarkMode() {
-      this.darkMode = !this.darkMode;
-    },
-  },
 })
 </script>
--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
 import router from "./routes/routes";
 import "tw-elements";
 import "./index.css";
 import "./assets/css/NotFound.css";
 
-createApp(App).use(router).mount("#app");
+library.add(faMoon, faSun);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
