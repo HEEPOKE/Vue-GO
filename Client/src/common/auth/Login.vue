aeb57116f97379a2c3d2b2f30ed38d72fb458229
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-27 09:44:09 +0700

    update auth page

--- Client/src/common/auth/Login.vue
+++ Client/src/common/auth/Login.vue
@@ -10,11 +10,13 @@
                     <HeaderLogin />
                     <form>
                         <div class="mb-6">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Email</label>
                             <input type="text"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                placeholder="Email address" />
+                                placeholder="Email" />
                         </div>
                         <div class="mb-6">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Password</label>
                             <input type="password"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="Password" />
@@ -37,12 +39,18 @@
                             class="inline-block px-7 py-3 mb-3 bg-blue-600 text-white font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full">
                             SIGN IN
                         </button>
-                        <button type="button"
-                            class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full"
-                            @click="goBack">
-                            BACK
-                        </button>
                     </form>
+                    <button type="button"
+                        class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full"
+                        @click="goBack">
+                        BACK
+                    </button>
+                    <p class="text-gray-800 mt-6 text-center">Not a member?
+                        <RouterLink to="/auth/register">
+                            <a
+                                class="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Register</a>
+                        </RouterLink>
+                    </p>
                     <div
                         class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
                         <p class="text-center font-semibold mx-4 mb-0">OR</p>
--- Client/src/common/auth/Register.vue
+++ Client/src/common/auth/Register.vue
@@ -9,27 +9,32 @@
                 <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
                     <HeaderRegister />
                     <form>
-                        <div class="mb-6">
+                        <div class="mb-3">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Username</label>
                             <input type="text"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="Username" />
                         </div>
-                        <div class="mb-6">
+                        <div class="mb-3">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Email</label>
                             <input type="email"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="Email" />
                         </div>
-                        <div class="mb-6">
+                        <div class="mb-3">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Password</label>
                             <input type="password"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="Password" />
                         </div>
-                        <div class="mb-6">
+                        <div class="mb-3">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Confirm Password</label>
                             <input type="password"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="Confirm Password" />
                         </div>
-                        <div class="mb-6">
+                        <div class="mb-3">
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Telephone</label>
                             <input type="tel"
                                 class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                 placeholder="xxx-xxx-xxxx" />
@@ -38,24 +43,24 @@
                             class="inline-block px-7 py-3 mb-3 bg-blue-600 text-white font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full">
                             SIGN UP
                         </button>
-                        <button type="button"
-                            class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full"
-                            @click="goBack">
-                            BACK
-                        </button>
-                        <div
-                            class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                            <p class="text-center font-semibold mx-4 mb-0">OR</p>
-                        </div>
-                        <a class="px-7 py-3 text-white font-medium text-sm rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-                            style="background-color: #3b5998" href="#!" role="button" data-mdb-ripple="true"
-                            data-mdb-ripple-color="light">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-3.5 h-3.5 mr-2">
-                                <path fill="currentColor"
-                                    d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-                            </svg>Continue with Facebook
-                        </a>
                     </form>
+                    <button type="button"
+                        class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full"
+                        @click="goBack">
+                        BACK
+                    </button>
+                    <div
+                        class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                        <p class="text-center font-semibold mx-4 mb-0">OR</p>
+                    </div>
+                    <a class="px-7 py-3 text-white font-medium text-sm rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+                        style="background-color: #3b5998" href="#!" role="button" data-mdb-ripple="true"
+                        data-mdb-ripple-color="light">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-3.5 h-3.5 mr-2">
+                            <path fill="currentColor"
+                                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+                        </svg>Continue with Facebook
+                    </a>
                 </div>
             </div>
         </div>
