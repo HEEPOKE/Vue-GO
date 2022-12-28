f8a1949bec0aaba5c223159fbf8f140d7affb315
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-28 14:53:49 +0700

    Update DataTableProduct.vue

--- Client/src/components/Product/DataTableProduct.vue
+++ Client/src/components/Product/DataTableProduct.vue
@@ -25,8 +25,7 @@
                         </thead>
                         <tbody>
                             <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                                v-for="Product in product" :key="product.id"
-                                >
+                                v-for="Product in product" :key="product.id">
                                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">1
                                 </td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
@@ -43,10 +42,14 @@
                                         <button type="button"
                                             class="inline-block mx-1 px-4 text-center py-2.5 bg-blue-600 text-white ont-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                             <font-awesome-icon icon="fa-solid fa-eye" class="w-4 h-3" /></button>
-                                        <button type="button"
-                                            class="inline-block mx-1 px-4 text-center py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">
-                                            <font-awesome-icon icon="fa-solid fa-pen-to-square"
-                                                class="w-4 h-3" /></button>
+                                        <routerLink :to="{
+    name: 'UpdateProductPage', params: { id: product.id }
+}">
+                                            <button type="button"
+                                                class="inline-block mx-1 px-4 text-center py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">
+                                                <font-awesome-icon icon="fa-solid fa-pen-to-square"
+                                                    class="w-4 h-3" /></button>
+                                        </routerLink>
                                         <button type="button"
                                             class="inline-block mx-1 px-4 text-center py-2.5 bg-red-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
                                             <font-awesome-icon icon="fa-solid fa-trash-can" class="w-4 h-3" /></button>
