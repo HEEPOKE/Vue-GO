9a2fc55bd232820b541188a26b3ff2d33c627053
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-28 10:45:25 +0700

    update product page

--- Client/src/common/Product.vue
+++ Client/src/common/Product.vue
@@ -1,13 +1,28 @@
 <template>
-  <ProductList />
+  <HeaderProduct />
+  <div class="flex justify-center mt-5">
+    <div class="block rounded-lg shadow-lg bg-white mx-auto">
+      <div class="py-3 px-6 border-b border-gray-300">
+        Product List
+      </div>
+      <AddProductBtn />
+      <div class="p-6 mt-8">
+        <DataTableProduct />
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import ProductList from "../components/Product/Index.vue";
+import HeaderProduct from '../components/Product/Header.vue';
+import DataTableProduct from '../components/Product/DataTableProduct.vue';
+import AddProductBtn from '../components/Product/AddProductBtn.vue';
 
 export default {
   name: "ProductsListCommon",
   components: {
-    ProductList,
+    HeaderProduct,
+    DataTableProduct,
+    AddProductBtn
   },
 };
 </script>
---
+++ Client/src/components/Product/AddProductBtn.vue
@@ -0,0 +1,67 @@
+<template>
+    <button type="button"
+        class="inline-block float-right px-6 py-3 my-2 mx-7 bg-blue-600 text-white ont-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        data-bs-toggle="modal" data-bs-target="#AddProduct">
+        <font-awesome-icon icon="fa-solid fa-plus" class="w-4 h-3 mr-3" />Create</button>
+
+    <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+        id="AddProduct" tabindex="-1" aria-labelledby="AddProductLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg relative w-auto pointer-events-none">
+            <div
+                class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                <div
+                    class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                    <h5 class="text-xl font-medium leading-normal text-gray-800" id="AddProductLabel">Modal title</h5>
+                    <button type="button"
+                        class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                        data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body relative p-4">
+                    Modal body text goes here.
+                </div>
+                <div
+                    class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+                    <button type="button" class="px-6
+          py-2.5
+          bg-purple-600
+          text-white
+          font-medium
+          text-xs
+          leading-tight
+          uppercase
+          rounded
+          shadow-md
+          hover:bg-purple-700 hover:shadow-lg
+          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-purple-800 active:shadow-lg
+          transition
+          duration-150
+          ease-in-out" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="px-6
+      py-2.5
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-blue-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out
+      ml-1">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+
+export default {
+    name: "AddProductBtn",
+}
+</script>
