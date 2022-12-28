63ace76fe9ff17e714aeb57961fd6a97e278b512
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-28 10:09:17 +0700

    update UI theme login page

--- Client/src/common/auth/Login.vue
+++ Client/src/common/auth/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="h-screen">
         <div class="container px-6 py-12 h-full">
-            <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800 dark:text-white">
                 <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
                         class="w-full" alt="Phone image" />
@@ -10,50 +10,50 @@
                     <HeaderLogin />
                     <form>
                         <div class="mb-6">
-                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Email</label>
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700 dark:text-white">Email</label>
                             <input type="text"
-                                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none dark:text-white"
                                 placeholder="Email" required />
                         </div>
                         <div class="mb-6">
-                            <label class="form-label inline-block text-lg mb-2 text-gray-700">Password</label>
+                            <label class="form-label inline-block text-lg mb-2 text-gray-700 dark:text-white">Password</label>
                             <input type="password"
-                                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none dark:text-white"
                                 placeholder="Password" required />
                         </div>
                         <div class="flex justify-between items-center mb-6">
                             <div class="form-group form-check">
                                 <input type="checkbox"
-                                    class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                    class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer dark:text-white"
                                     id="exampleCheck3" checked />
-                                <label class="form-check-label inline-block text-gray-800" for="exampleCheck2">Remember
+                                <label class="form-check-label inline-block text-gray-800 dark:text-white" for="exampleCheck2">Remember
                                     me</label>
                             </div>
                             <RouterLink to="/auth/forgotPassword">
                                 <a
-                                    class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">Forgot
+                                    class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out dark:text-white">Forgot
                                     password?</a>
                             </RouterLink>
                         </div>
                         <button type="submit"
-                            class="inline-block px-7 py-3 mb-3 bg-blue-600 text-white font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full">
+                            class="inline-block px-7 py-3 mb-3 bg-blue-600 text-white font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full dark:text-white">
                             SIGN IN
                         </button>
                     </form>
                     <button type="button"
-                        class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full"
+                        class="inline-block px-7 py-3 bg-gray-300 text-gray-700 font-medium text-sm rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-full dark:text-white dark:bg-rose-600"
                         @click="goBack">
                         BACK
                     </button>
-                    <p class="text-gray-800 mt-6 text-center">Not a member?
+                    <p class="text-gray-800 mt-6 text-center dark:text-white">Not a member?
                         <RouterLink to="/auth/register">
                             <a
-                                class="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out">Register</a>
+                                class="text-blue-600 hover:text-blue-700 focus:text-blue-700 transition duration-200 ease-in-out dark:text-rose-600">Register</a>
                         </RouterLink>
                     </p>
                     <div
-                        class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                        <p class="text-center font-semibold mx-4 mb-0">OR</p>
+                        class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5 dark:text-white">
+                        <p class="text-center font-semibold mx-4 mb-0 dark:text-white">OR</p>
                     </div>
                     <a class="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
                         style="background-color: #3b5998" role="button">
--- Client/src/components/auth/login/HeaderLogin.vue
+++ Client/src/components/auth/login/HeaderLogin.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center text-gray-800">
+    <div class="text-center text-gray-800 dark:text-white">
         <h1 class="text-4xl font-bold mb-5">Login</h1>
     </div>
 </template>
