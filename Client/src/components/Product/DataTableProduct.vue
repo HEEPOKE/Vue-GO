d9b66749349456190f31d5fc259f4f51d34edbd9
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-28 12:13:06 +0700

    update icon manage

--- Client/src/components/Product/DataTableProduct.vue
+++ Client/src/components/Product/DataTableProduct.vue
@@ -37,15 +37,18 @@
                                     @mdo
                                 </td>
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    <div class="flex space-x-2 justify-center">
-                                        <div>
-                                            <button type="button"
-                                                class="inline-block mx-1 px-6 py-2.5 bg-blue-600 text-white ont-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Primary</button>
-                                            <button type="button"
-                                                class="inline-block mx-1 px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">Warning</button>
-                                            <button type="button"
-                                                class="inline-block mx-1 px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Danger</button>
-                                        </div>
+                                    <div class="justify-center">
+                                        <button type="button"
+                                            class="inline-block mx-1 px-4 text-center py-2.5 bg-blue-600 text-white ont-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                            <font-awesome-icon icon="fa-solid fa-eye" class="w-4 h-3" /></button>
+                                        <button type="button"
+                                            class="inline-block mx-1 px-4 text-center py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">
+                                            <font-awesome-icon icon="fa-solid fa-pen-to-square"
+                                                class="w-4 h-3" /></button>
+                                        <button type="button"
+                                            class="inline-block mx-1 px-4 text-center py-2.5 bg-red-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">
+                                            <font-awesome-icon icon="fa-solid fa-trash-can"
+                                                class="w-4 h-3" /></button>
                                     </div>
                                 </td>
                             </tr>
