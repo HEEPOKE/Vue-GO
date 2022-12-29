98c85ddaedc9d9db351fc216a0bdaaeeea407f1f
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-29 10:24:50 +0700

    Update authcontroller.go

--- Server/controllers/auth/authcontroller.go
+++ Server/controllers/auth/authcontroller.go
@@ -1,1 +1,14 @@
 package auth
+
+import (
+	"Server/Api/database"
+	"Server/Api/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func ReadProduct(c *fiber.Ctx) {
+	var product []models.Product
+	database.DB.First(&product)
+	c.JSON(fiber.StatusOK, product)
+}
