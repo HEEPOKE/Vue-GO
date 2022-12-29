d246837148e88ba4f13c52d267c6709085f7a8e6
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-29 14:23:04 +0700

    Update routes.go

--- Server/routes/routes.go
+++ Server/routes/routes.go
@@ -2,12 +2,22 @@ package routes
 
 import (
 	productController "Server/Api/controllers/product"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Router() {
 	r := fiber.New()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     os.Getenv("ENDPOINT_URL"),
+		AllowMethods:     "GET,POST,PUT,DELETE",
+		AllowHeaders:     "Origin, Content-Type",
+		ExposeHeaders:    "Content-Length",
+		AllowCredentials: true,
+		MaxAge:           12,
+	}))
 
 	r.Get("/product", productController.ReadProduct)
 	r.Post("/product/add", productController.AddProduct)
