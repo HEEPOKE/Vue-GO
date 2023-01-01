9009201d9fbe3bab0835e1fa19be84f34b97a9fb
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-01-01 20:22:12 +0700

    update

--- Client/src/common/Product.vue
+++ Client/src/common/Product.vue
@@ -1,28 +1,28 @@
 <template>
-  <HeaderProduct />
-  <div class="flex justify-center mt-5">
-    <div class="block rounded-lg shadow-lg bg-white mx-auto">
-      <div class="py-3 px-6 border-b border-gray-300">
-        Product List
-      </div>
-      <AddProductBtn />
-      <div class="p-6 mt-8">
-        <DataTableProduct />
+  <div>
+    <HeaderProduct />
+    <div class="flex justify-center mt-5">
+      <div class="block rounded-lg shadow-lg bg-white mx-auto">
+        <div class="py-3 px-6 border-b border-gray-300">Product List</div>
+        <AddProductBtn />
+        <div class="p-6 mt-8">
+          <DataTableProduct />
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import HeaderProduct from '../components/Product/Header.vue';
-import DataTableProduct from '../components/Product/DataTableProduct.vue';
-import AddProductBtn from '../components/Product/AddProductBtn.vue';
+import HeaderProduct from "../components/Product/Header.vue";
+import DataTableProduct from "../components/Product/DataTableProduct.vue";
+import AddProductBtn from "../components/Product/AddProductBtn.vue";
 
 export default {
   name: "ProductsListCommon",
   components: {
     HeaderProduct,
     DataTableProduct,
-    AddProductBtn
+    AddProductBtn,
   },
 };
 </script>
--- Client/src/components/navbar/Index.vue
+++ Client/src/components/navbar/Index.vue
@@ -1,51 +1,88 @@
 <template>
   <nav
-    class="relative w-full flex flex-wrap items-center justify-between py-3 bg-rose-600 text-white shadow-lg navbar navbar-expand-lg navbar-light dark:bg-black dark:text-gray-700 dark:border-b dark:border-b-white">
-    <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+    class="relative w-full flex flex-wrap items-center justify-between py-3 bg-rose-600 text-white shadow-lg navbar navbar-expand-lg navbar-light dark:bg-black dark:text-gray-700 dark:border-b dark:border-b-white"
+  >
+    <div
+      class="container-fluid w-full flex flex-wrap items-center justify-between px-6"
+    >
       <button
         class="navbar-toggler text-gray-200 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
-        type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="w-6" role="img"
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-          </path>
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent1"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="bars"
+          class="w-6"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+          ></path>
         </svg>
       </button>
-      <div class="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent1">
-        <Router-link to="/" class="text-xl text-white pr-2 font-semibold dark:text-slate-100 dark:hover:text-white">
+      <div
+        class="collapse navbar-collapse flex-grow items-center"
+        id="navbarSupportedContent1"
+      >
+        <router-link
+          to="/"
+          class="text-xl text-white pr-2 font-semibold dark:text-slate-100 dark:hover:text-white"
+        >
           Topic
-        </Router-link>
+        </router-link>
         <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
           <li class="nav-item p-2">
-            <RouterLink to="/" class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white">
+            <router-link
+              to="/"
+              class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white"
+            >
               Dashboard
-            </RouterLink>
+            </router-link>
           </li>
           <li class="nav-item p-2">
-            <RouterLink to="/product" class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white">
+            <router-link
+              to="/product"
+              class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white"
+            >
               Product
-            </RouterLink>
+            </router-link>
           </li>
           <li class="nav-item p-2">
-            <RouterLink to="/about" class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white">
+            <router-link
+              to="/about"
+              class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white"
+            >
               About
-            </RouterLink>
+            </router-link>
           </li>
         </ul>
         <div class="flex items-center relative">
           <ul class="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
             <li class="nav-item p-2">
-              <RouterLink to="/auth/login" class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white">
+              <router-link
+                to="/auth/login"
+                class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white"
+              >
                 Login
-              </RouterLink>
+              </router-link>
             </li>
             <li class="nav-item p-2">
-              <RouterLink to="/auth/register"
-                class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white">
+              <router-link
+                to="/auth/register"
+                class="nav-link text-white opacity-60 hover:opacity-80 focus:opacity-80 p-0 dark:text-slate-100 dark:hover:text-white"
+              >
                 Register
-              </RouterLink>
+              </router-link>
             </li>
           </ul>
           <ChangeModeButton />
@@ -55,12 +92,12 @@
   </nav>
 </template>
 <script lang="ts">
-import ChangeModeButton from './ChangeMode.vue';
+import ChangeModeButton from "./ChangeMode.vue";
 
 export default {
   name: "NavBarVue",
   components: {
     ChangeModeButton,
-  }
+  },
 };
 </script>
