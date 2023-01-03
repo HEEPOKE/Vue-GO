54e3f2722c4bab870e606b0f71e475d3ef68d344
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-03 14:16:57 +0700

    Update validation.middleware.go

--- Server/middleware/validation.middleware.go
+++ Server/middleware/validation.middleware.go
@@ -1,1 +1,42 @@
 package middleware
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func ValidationMiddleware(next fiber.Handler) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		hmacSampleSecret := []byte(os.Getenv("JWT_SECRET_KEY"))
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).SendString("Missing authorization header")
+		}
+
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			return c.Status(fiber.StatusUnauthorized).SendString("Invalid authorization header")
+		}
+		tokenString := parts[1]
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return hmacSampleSecret, nil
+		})
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Locals("userId", claims)
+		} else {
+			c.Status(fiber.StatusUnauthorized).JSON(&fiber.Map{
+				"status":  "forbidden",
+				"message": err.Error(),
+			})
+		}
+		return next(c)
+	}
+}
